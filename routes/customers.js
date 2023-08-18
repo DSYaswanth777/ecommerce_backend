58b38babbf1e2472200d5ea02fc55cd22cf4d9ed
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-08-18 23:07:52 +0530

    add(Search Customer)

--- routes/customers.js
+++ routes/customers.js
@@ -14,5 +14,38 @@ router.get('/customers', async (req, res) => {
     res.status(403).json({ message: 'Access denied' });
   }
 });
+router.get('/search/customer', async (req, res) => {
+  if (req.isAuthenticated() && req.user.role === 'admin') {
+    try {
+      const { searchField, searchTerm } = req.query;
+      let query = { role: 'customer' };
+
+      if (searchField && searchTerm) {
+        const regex = new RegExp(searchTerm, 'i'); // Case-insensitive search
+        switch (searchField) {
+          case 'name':
+            query.name = regex;
+            break;
+          case 'email':
+            query.email = regex;
+            break;
+          case 'mobile':
+            query.mobile = regex;
+            break;
+          default:
+            break;
+        }
+      }
+
+      const customers = await User.find(query, 'name mobile email');
+      res.status(200).json(customers);
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while fetching customers' });
+    }
+  } else {
+    res.status(403).json({ message: 'Access denied' });
+  }
+});
 
 module.exports = router;
+
