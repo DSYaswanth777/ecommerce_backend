76f2873b4486e89106b964f02fd04f0f1433b4bd
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-09-17 01:27:17 +0530

    Add(Cloudanry to the AddProduct API)

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,6 +4,13 @@ const Product = require("../models/productModel");
 const multer = require("multer"); 
 //** Destination for uploaded files */
 const upload = multer({ dest: "uploads/" }); 
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 //** Add Product controller */
 exports.addProduct = [
   upload.array("productImages", 5), // Assuming a maximum of 5 images can be uploaded
@@ -20,15 +27,18 @@ exports.addProduct = [
       } = req.body;
 
       const productPrice = productMRP - productDiscount;
-
-      const productImages = req.files.map((file) => file.path);
-
+      const uploadedImages = await Promise.all(
+        req.files.map(async (file) => {
+          const result = await cloudinary.uploader.upload(file.path);
+          return result.secure_url;
+        })
+      );
       const newProduct = await Product.create({
         productName,
         productMRP,
         productDiscount,
         productPrice,
-        productImages,
+        productImages:uploadedImages,
         productInfo,
         productColorOptions,
         subcategoryId,
@@ -83,10 +93,27 @@ exports.editProduct = [
         return res.status(404).json({ message: "Product not found" });
       }
 
-      // Apply updates to the existing product object
+      // Delete existing images from Cloudinary
+      for (const imageUrl of existingProduct.productImages) {
+        const public_id = imageUrl.split('/').pop().split('.')[0];
+        await cloudinary.uploader.destroy(public_id);
+      }
+
+      // Upload and replace with new images in Cloudinary
+      const uploadedImages = await Promise.all(
+        req.files.map(async (file) => {
+          const result = await cloudinary.uploader.upload(file.path);
+          return result.secure_url;
+        })
+      );
+
+      // Update the product details
       for (const [key, value] of Object.entries(updatedFields)) {
         existingProduct[key] = value;
       }
+
+      // Update productImages with new Cloudinary URLs
+      existingProduct.productImages = uploadedImages;
 
       // Calculate updated productPrice if applicable
       if (
@@ -110,17 +137,31 @@ exports.editProduct = [
     }
   },
 ];
+
 //**  Delete Product controller
 exports.deleteProduct = [
   async (req, res) => {
     try {
-      const deletedProduct = await Product.findByIdAndDelete(
-        req.params.productId
-      );
+      const productId = req.params.productId;
+
+      // Fetch the product from the database
+      const deletedProduct = await Product.findById(productId);
 
       if (!deletedProduct) {
         return res.status(404).json({ message: "Product not found" });
       }
+
+      // Delete images from Cloudinary
+      for (const imageUrl of deletedProduct.productImages) {
+        // Extract the public_id from the Cloudinary URL
+        const public_id = imageUrl.split('/').pop().split('.')[0];
+        
+        // Delete the image from Cloudinary
+        await cloudinary.uploader.destroy(public_id);
+      }
+
+      // Delete the product from MongoDB
+      await Product.findByIdAndDelete(productId);
 
       res.status(200).json({ message: "Product deleted successfully" });
     } catch (error) {
